27a4fee9d2a3ce5baeaf0bdefc01681f234f32b5
Rahul Pal <usefulltech1819@gmail.com>
2023-06-26 14:05:19 +0530

    Error fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import './App.css'
+import './App.scss'
 import { fetchDataFromapi } from './utils/api'
 import { useSelector, useDispatch } from 'react-redux'
 import { getApiConfiguration, getGenres } from './store/homeSlice'
@@ -10,7 +10,7 @@ import Home from './pages/home/Home'
 import Details from './pages/details/Details'
 import SearchResult from './pages/searchResult/SearchResult'
 import Explore from './pages/explore/Explore'
-import PageNotFound from './pages/404/pageNotFound'
+import PageNotFound from './pages/404/PageNotFound'
 
 function App() {
   const dispatch = useDispatch();
