cf585966ba17dae6a0a659c282eb19874a6df0f6
Rahul Pal <usefulltech1819@gmail.com>
2023-06-26 14:22:17 +0530

    Error fix

--- src/components/caousel/Carousel.jsx
+++ src/components/caousel/Carousel.jsx
@@ -7,7 +7,7 @@ import { useSelector } from "react-redux";
 import dayjs from "dayjs";
 
 import ContentWrapper from "../contentWrapper/ContentWrapper";
-import Img from "../lazyLoadImage/Img";
+import Img from "../lazyLoadImage/img";
 import PosterFallback from "../../assets/no-poster.png";
 import CircleRating from "../circleRating/CircleRating";
 
